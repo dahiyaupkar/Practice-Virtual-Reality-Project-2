75ced6f5185a0125daa8d3dfc02babab3c6804fe
Upkar Dahiya <dahiyaupkar@gmail.com>
2019-05-03 17:18:33 -0700

    Uploaded Project Files

---
+++ Assets/Script/PopUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject uiObject;
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+    void OnTriggerEnter(Collider player)
+    {
+        if(player.gameObject.tag=="Player")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+    IEnumerable WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(uiObject);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
